464a7663412bac59fd87920ccf9d13ae11c17666
DryKillLogic <drykilllogic@gmail.com>
2023-11-20 22:20:23 -0500

    Remove results

--- addon.js
+++ addon.js
@@ -29,14 +29,7 @@ const builder = new addonBuilder(manifest)
 builder.defineCatalogHandler(({type, id, extra}) => {
 	console.log("request for catalogs: "+type+" "+id)
 	// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineCatalogHandler.md
-	return Promise.resolve({ metas: [
-		{
-			id: "tt1254207",
-			type: "movie",
-			name: "The Big Buck Bunny",
-			poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg"
-		}
-	] })
+	return Promise.resolve({ metas: [{}] })
 })
 
 module.exports = builder.getInterface()

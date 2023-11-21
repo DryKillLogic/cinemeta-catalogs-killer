d9b0a7e098addb2f1fd1333fcacf2016c4fac666
DryKillLogic <drykilllogic@gmail.com>
2023-11-20 22:10:45 -0500

    Initial commit

---
+++ addon.js
@@ -0,0 +1,42 @@
+const { addonBuilder } = require("stremio-addon-sdk")
+
+// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
+const manifest = {
+	"id": "com.linvo.cinemeta",
+	"version": "0.0.1",
+	"catalogs": [
+		{
+			"type": "movie",
+			"id": "top"
+		},
+		{
+			"type": "series",
+			"id": "top"
+		}
+	],
+	"resources": [
+		"catalog"
+	],
+	"types": [
+		"movie",
+		"series"
+	],
+	"name": "cinemeta",
+	"description": "Cinemeta Catalogs Killer"
+}
+const builder = new addonBuilder(manifest)
+
+builder.defineCatalogHandler(({type, id, extra}) => {
+	console.log("request for catalogs: "+type+" "+id)
+	// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/requests/defineCatalogHandler.md
+	return Promise.resolve({ metas: [
+		{
+			id: "tt1254207",
+			type: "movie",
+			name: "The Big Buck Bunny",
+			poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg"
+		}
+	] })
+})
+
+module.exports = builder.getInterface()
